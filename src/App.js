8ce92284b79c48e077bc7fa55b413b659efcd852
iqbalrizan <iqbalrizan@gmail.com>
2022-12-30 15:12:24 +0700

    initial-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const questions = [
+    {
+      questionText: "In what US city are the baseball team the White Sox based?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "Cincinnati", isCorrect: false },
+        { answerText: "Chicago", isCorrect: true },
+        { answerText: "Kansas", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What position did Babe Ruth begin his career playing?",
+      answerOptions: [
+        { answerText: "Catcher", isCorrect: false },
+        { answerText: "Pitcher", isCorrect: true },
+        { answerText: "Outfield", isCorrect: false },
+        { answerText: "Designated Hitter", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the nickname of the baseball team based in the US city of Toronto?",
+      answerOptions: [
+        { answerText: "Blue Jays", isCorrect: true },
+        { answerText: "Angels", isCorrect: false },
+        { answerText: "Red Sox", isCorrect: false },
+        { answerText: "Astros", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "When Shohei Ohtani was born ?",
+      answerOptions: [
+        { answerText: "3 July 1994", isCorrect: false },
+        { answerText: "4 July 1994", isCorrect: false },
+        { answerText: "6 July 1994", isCorrect: false },
+        { answerText: "5 July 1994", isCorrect: true },
+      ],
+    },
+    {
+      questionText: "What division are major league baseball team the Chicago Cubs in?",
+      answerOptions: [
+        { answerText: "Central", isCorrect: true },
+        { answerText: "North", isCorrect: false },
+        { answerText: "West", isCorrect: false },
+        { answerText: "East", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What team lost a record 113 games in 2003?",
+      answerOptions: [
+        { answerText: "Philadhelpia Philies", isCorrect: false },
+        { answerText: "LA Dodgers", isCorrect: false },
+        { answerText: "New York Yankees", isCorrect: true },
+        { answerText: "San Fransisco Giants", isCorrect: false },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [score, setScore] = useState(0);
+  const handleClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+      alert("CONGRATS!!");
+    }
+  };
+
+  const [showScore, setShowScore] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>MLB QUIZ {currentQuestion + 1}</span> of {questions.length}
+            </div>
+            <div className="question-text">{questions[currentQuestion].questionText}</div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((data) => (
+              <button onClick={() => handleClick(data.isCorrect)}>{data.answerText}</button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
